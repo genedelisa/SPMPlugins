2cd55fe4fe92c9da4fc6bd2439b04d833c66726a
Gene <genedelisa@gmail.com>
2023-04-11 07:00:55 -0400

    exact swift-format version

--- Package.swift
+++ Package.swift
@@ -10,29 +10,29 @@ let package = Package(
         // Ventura 13.2.1
         .macOS(.v13)
     ],
-    products: [
-        .plugin(
-            name: "Reformat Source Code",
-            targets: [
-                "Reformat Source Code"
-            ]
-        ),
-        .plugin(
-            name: "Regenerate Contributors List",
-            targets: [
-                "Regenerate Contributors List"
-            ]
-        ),
-        .plugin(
-            name: "Update Copyright Dates",
-            targets: [
-                "Update Copyright Dates"
-            ]
-        ),
-        
-    ],
+//    products: [
+//        .plugin(
+//            name: "Reformat Source Code",
+//            targets: [
+//                "Reformat Source Code"
+//            ]
+//        ),
+//        .plugin(
+//            name: "Regenerate Contributors List",
+//            targets: [
+//                "Regenerate Contributors List"
+//            ]
+//        ),
+//        .plugin(
+//            name: "Update Copyright Dates",
+//            targets: [
+//                "Update Copyright Dates"
+//            ]
+//        ),
+//        
+//    ],
     dependencies: [
-
+        .package(url: "https://github.com/apple/swift-format", branch: "release/5.7")
     ],
     targets: [
         
@@ -100,25 +100,25 @@ let package = Package(
 )
 
 // https://github.com/apple/swift-format#matching-swift-format-to-your-swift-version-swift-57-and-earlier
-
-#if swift(>=5.7)
-package.dependencies += [
-    .package(url: "https://github.com/apple/swift-format", branch: "release/5.7")
-]
-#elseif swift(>=5.6)
-package.dependencies += [
-    .package(url: "https://github.com/apple/swift-format", branch: "release/5.6")
-]
-#elseif swift(>=5.5)
-package.dependencies += [
-    .package(url: "https://github.com/apple/swift-format", branch: "swift-5.5-branch")
-]
-#elseif swift(>=5.4)
-package.dependencies += [
-    .package(url: "https://github.com/apple/swift-format", branch: "swift-5.4-branch")
-]
-#elseif swift(>=5.3)
-package.dependencies += [
-    .package(url: "https://github.com/apple/swift-format", branch: "swift-5.3-branch")
-]
-#endif
+//
+//#if swift(>=5.7)
+//package.dependencies += [
+//    .package(url: "https://github.com/apple/swift-format", branch: "release/5.7")
+//]
+//#elseif swift(>=5.6)
+//package.dependencies += [
+//    .package(url: "https://github.com/apple/swift-format", branch: "release/5.6")
+//]
+//#elseif swift(>=5.5)
+//package.dependencies += [
+//    .package(url: "https://github.com/apple/swift-format", branch: "swift-5.5-branch")
+//]
+//#elseif swift(>=5.4)
+//package.dependencies += [
+//    .package(url: "https://github.com/apple/swift-format", branch: "swift-5.4-branch")
+//]
+//#elseif swift(>=5.3)
+//package.dependencies += [
+//    .package(url: "https://github.com/apple/swift-format", branch: "swift-5.3-branch")
+//]
+//#endif
